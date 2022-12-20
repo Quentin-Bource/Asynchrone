572c108496d972f5a093fc8c6f6acf9d6a539819
Quentin <quentin01.bource@gmail.com>
2022-12-20 12:20:56 +0100

    finish basique

--- script.js
+++ script.js
@@ -5,22 +5,37 @@ function cit () {
   fetchQuote()
 	.then((response) => response.json())
 	.then((json) => {
-        
+        //createquota
         let newDiv = document.createElement('div')
         newDiv.classList.add("quota")
-        newDiv.appendChild(document.createTextNode('"'+ json.quote +'"' + json.author))
-        document.body.appendChild(newDiv) 
+        newDiv.appendChild(document.createTextNode('"'+ json.quote +'"   ' + json.author))
+        document.getElementById('citation').appendChild(newDiv) 
+
+      //createimage
         let newPhoto = document.createElement('div')
         newPhoto.classList.add("picture")
-        newPhoto.style.background = ("url(" + json.photo + ") center no-repeat")
+        if (json.photo){
+        newPhoto.style.background = ("url(" + json.photo + ") center no-repeat")}
+        else 
+        {
+        newPhoto.style.background = ("url(/no_picture.png) center no-repeat")}
+        newPhoto.style.backgroundSize = ("cover")
         newDiv.appendChild(newPhoto)
-        console.log(newPhoto)
         
+       
 
+
+        
 
 	})
 	.catch((error) => {
 		console.log("There was an error!", error);
-	});}
+	});
 
-    document.getElementById('click').addEventListener('click', cit)
+  document.getElementById('click').addEventListener('click', cit)
+  document.getElementById('citation').firstChild.remove()
+
+
+}
+
+  cit() 
